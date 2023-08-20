976662ee4a8cb997d1e69629faebeaa4932f8b0d
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-08-20 16:33:13 +0300

    edit controller for boards

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def render_404
+    render file: "#{Rails.root}/public/404.html", layout: true, status: :not_found
+  end
 end
--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,15 @@
 class BoardsController < ApplicationController
   def index
+    @boards = Board.all
   end
 
   def show
+    @board = Board.find_by(name: params[:id])
+    if @board
+      @threads = Thread.all
+      render "show"
+    else
+      render_404
+    end
   end
 end
