2f0e11bbd490eeafc09bddfed0cec47c520975d2
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-10-06 13:20:35 +0300

    add db index for board names

--- db/migrate/20230820113804_create_boards.rb
+++ db/migrate/20230820113804_create_boards.rb
@@ -1,7 +1,7 @@
 class CreateBoards < ActiveRecord::Migration[7.0]
   def change
     create_table :boards do |t|
-      t.string :name, null: false
+      t.string :name, null: false, index: { unique: true }
 
       t.timestamps
     end
