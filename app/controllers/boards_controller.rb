36b6106058d1dbcf5c6baedfc2ffaf4dec82ab65
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-10-15 17:28:07 +0300

    optimize n+1 SQL for board view

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -5,10 +5,20 @@ class BoardsController < ApplicationController
 
   def show
     @board = Board.find_by(name: params[:name])
+
     raise ActiveRecord::RecordNotFound if @board.nil?
 
-    @topics = @board.topics.order(updated_at: :desc).page(params[:page])
+    @topics = @board.topics
+                    .includes(posts_its_images_and_their_variants)
+                    .where
+                    .order(updated_at: :desc).page(params[:page])
+
     # ? How to include a couple (not all) of preview posts with #include(:posts)?
-    # @topics = TopicDecorator.decorate_collection(@topics.all)
+  end
+
+  private
+
+  def posts_its_images_and_their_variants
+    { posts: [:images_attachments, { images_blobs: { variant_records: { image_attachment: :blob } } }] }
   end
 end
