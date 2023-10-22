f9e23e5a2d5def6f86864ed514cf2c819e293b4b
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-10-22 18:45:53 +0300

    turn off unnecessary tests

--- spec/requests/boards_spec.rb
+++ spec/requests/boards_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Boards", type: :request do
   describe "GET /index" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/boards/index"
       expect(response).to have_http_status(:success)
     end
--- spec/requests/topics_spec.rb
+++ spec/requests/topics_spec.rb
@@ -2,28 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "Topics", type: :request do
   describe "GET /index" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/topics/index"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/topics/show"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /new" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/topics/new"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /create" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/topics/create"
       expect(response).to have_http_status(:success)
     end
