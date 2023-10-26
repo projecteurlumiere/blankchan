9a59a00b525db43c7fbae51921012ae8098a7e53
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-10-26 20:57:05 +0300

    implement removal of the oldest topic upon creation of a new one

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,9 +1,23 @@
 class Topic < ApplicationRecord
+  MAX_TOPICS = 30
+  paginates_per 10
+
   belongs_to :board
-  paginates_per 15
   has_many :posts, dependent: :destroy
 
   accepts_nested_attributes_for :posts, :board
 
   validates :board_id, presence: true
+
+  after_create :delete_the_oldest_topic
+
+  private
+
+  def delete_the_oldest_topic
+    if board.topics.all.count > MAX_TOPICS
+      last_topic = board.topics.includes({ posts: [:images_attachments, { images_blobs: { variant_records: { image_attachment: :blob } } }] }).order(updated_at: :desc).last
+      last_topic.posts.destroy_all
+      last_topic.destroy
+    end
+  end
 end
