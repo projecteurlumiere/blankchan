77bf86b02996082e79c339b29a77f7a3653c902f
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-11-03 15:25:11 +0300

    fix posts_not being created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.build(post_params)
     @post.author_ip = request.remote_ip
-    @post.author_status = current_user&.id || nil
+    @post.author_id = current_user&.id
     if @post.save && @post.images.attach(params[:images])
       flash.notice = "Post created successfully"
       redirect_to board_topic_path(params[:board_name],
