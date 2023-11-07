5989a7b31317e75c3d7c31f98b5a233d54d53483
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-11-07 18:06:26 +0300

    enhance (non-styled) new post form with sticky position

--- app/javascript/controllers/reply_controller.js
+++ app/javascript/controllers/reply_controller.js
@@ -3,8 +3,21 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "formTextField" ]
 
+  connect(){
+    this.form = document.querySelector("#new-post-form-container");
+  }
+
   pastePostId(e){
     this.#pasteTextAtCaret(this.formTextFieldTarget, `>>${e.params.id}\n`);
+    if (e.ctrlKey == false) this.openForm();
+  }
+
+  openForm(){
+    this.form.classList.add("new-post-form-container-active");
+  }
+
+  closeForm(){
+    this.form.classList.remove("new-post-form-container-active");
   }
 
   #pasteTextAtCaret(textElement, text) {
