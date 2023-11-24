88606bfdd132ccd16836e126c795b41a07d38d28
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-11-24 20:13:34 +0300

    style topics & post (except image showcase)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def truncate_image_name(image, n = 5, omission = "...")
+    reversed_name = image.filename.to_s.reverse
+    reversed_extension = reversed_name.match(/\w+\./).to_s
+    "#{reversed_name.sub(reversed_extension, "").reverse.truncate(n, omission: omission)}#{reversed_extension.reverse}"
+  end
 end
