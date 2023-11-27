b8868c848017300489a639316b819953d60892ea
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-11-27 12:36:17 +0300

    add & style pagination (with turbo streams; no js side yet)

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -19,6 +19,13 @@ class BoardsController < ApplicationController
     #                 .order(updated_at: :desc).page(params[:page])
 
     # ? How to include a couple (not all) of preview posts with #include(:posts)?
+
+    render :show and return if params[:page].nil? || params[:page] == 1
+
+    respond_to do |format|
+      format.html { render :show }
+      format.turbo_stream { render :show }
+    end
   end
 
   private
